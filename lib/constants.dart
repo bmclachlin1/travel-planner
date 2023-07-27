f6114d20cbbd84c7455118e0b5b2de443490d5db
Blake <blake@alertlabs.com>
2023-07-27 14:23:23 -0600

    add constants

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,16 @@
-class Constants {
-  static String openWeatherMapApiKey = "1fcff69704f9484b34fba760af254417";
+class Api {
+  static const String openWeatherMapApiKey = "1fcff69704f9484b34fba760af254417";
+}
+
+class Sizes {
+  static const double xs = 4.0;
+  static const double small = 8.0;
+  static const double medium = 16.0;
+  static const double large = 24.0;
+  static const double xl = 32.0;
+
+  static const double containerWidth = 450;
+  static const double containerHeight = 300;
+
+  static const double borderWidth = 2.0;
 }
